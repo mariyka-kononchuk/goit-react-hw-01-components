bbfd151dce77fb616a3bb392daf40c1b4d39ea53
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-08-29 13:27:34 +0300

    component statistics created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import Profile from './components/profile';
-import user from './social-profile/user.json';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 export default function App() {
   return (
     <div>
@@ -12,6 +14,7 @@ export default function App() {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
     </div>
   );
 }
---
+++ src/components/Statistics.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section class="statistics">
+      {/* checking existence of Title */}
+      {title && <h2 class="title">{title}</h2>}
+      <ul class="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li class="item" key={id}>
+            <span class="label">{label}</span>
+            <span class="percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+};
