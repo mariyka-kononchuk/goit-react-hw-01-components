c74e13e560e148fde54a3f9c51bff027a079d5e6
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-08-29 18:33:10 +0300

    Profile component syles corrected

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
-import './Profile.css';
-
+import styles from './Profile.module.css';
+console.log(styles);
 export default function Profile({ avatar, tag, name, location, followers, views, likes }) {
   return (
-    <div className="Profile">
-      <div className="Profile__description">
-        <img src={avatar} alt={tag} className="Profile__avatar" />
-        <p className="Profile__name">{name}</p>
-        <p className="Profile__tag">{tag}</p>
-        <p className="Profile__location">{location}</p>
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt={tag} className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
       </div>
 
-      <ul className="Profile__stats">
-        <li className="Profile__stats--item">
-          <span className="Profile__stats--label">Followers</span>
-          <span className="Profile__stats--quantity">{followers}</span>
+      <ul className={styles.stats}>
+        <li className={styles.item}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{followers}</span>
         </li>
-        <li className="Profile__stats--item">
-          <span className="Profile__stats--label">Views</span>
-          <span className="Profile__stats--quantity">{views}</span>
+        <li className={styles.item}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{views}</span>
         </li>
-        <li className="Profile__stats--item">
-          <span className="Profile__stats--label">Likes</span>
-          <span className="Profile__stats--quantity">{likes}</span>
+        <li className={styles.item}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
