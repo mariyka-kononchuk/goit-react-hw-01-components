a162f990b847dcab07e3e3b170d4d125aa1acc93
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-08-29 20:30:02 +0300

    TransactionHistory component styles added

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -4,19 +4,19 @@ import s from './TransactionHistory.module.css';
 export default function TransactionHistory({ items }) {
   return (
     <table className={s.transactionHistory}>
-      <thead>
+      <thead className={s.head}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={s.title}>Type</th>
+          <th className={s.title}>Amount</th>
+          <th className={s.title}>Currency</th>
         </tr>
       </thead>
       <tbody>
         {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+          <tr key={id} className={s.item}>
+            <td className={s.type}>{type}</td>
+            <td className={s.amount}>{amount}</td>
+            <td className={s.currency}>{currency}</td>
           </tr>
         ))}
       </tbody>
