8867bc95da7ca0a400ade9b11a24f572c41b4edb
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-08-30 10:15:22 +0300

    Profile component - mistake corrected

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import s from './Profile.module.css';
 
-export default function Profile({ avatar, tag, name, location, followers, views, likes }) {
+export default function Profile({ avatar, tag, name, location, stats }) {
   return (
     <div className={s.profile}>
       <div className={s.description}>
@@ -14,15 +14,15 @@ export default function Profile({ avatar, tag, name, location, followers, views,
       <ul className={s.stats}>
         <li className={s.item}>
           <span className={s.label}>Followers</span>
-          <span className={s.quantity}>{followers}</span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
         <li className={s.item}>
           <span className={s.label}>Views</span>
-          <span className={s.quantity}>{views}</span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
         <li className={s.item}>
           <span className={s.label}>Likes</span>
-          <span className={s.quantity}>{likes}</span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
